1441f8443522343278b2e49e1c821a09fb114855
koutschan <christoph.koutschan@ricam.oeaw.ac.at>
2018-10-23 14:36:15 +0200

    Update Mathematica package and create demo notebook

--- ZonalPolynomials.m
+++ ZonalPolynomials.m
@@ -1,17 +1,39 @@
-\[Rho] = Rho; 
-Rho[lam_] := Total[MapIndexed[#1 * (#1 - #2[[1]])&, lam]];
+(*  ZonalPolynomials -- calculation of zonal polynomials
+ *  Copyright (C) 2018  Christoph Koutschan
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version, see http://www.gnu.org/licenses/
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *)
 
-pgr[lam_, mu_] :=
+
+(* The function rho. Can be written as Rho or with the greek letter. *)
+\[Rho] = Rho;
+Rho[lam_List] := Total[MapIndexed[#1 * (#1 - #2[[1]])&, lam]];
+
+(* Whether partition lam is (strictly) greater than partition mu, in lexicographical order *)
+PartitionGreater[lam_List, mu_List] :=
   MatchQ[#1 - #2 & @@ PadRight[{lam, mu}], {(0) ..., a_ /; a > 0, ___}];
-pgeq[lam_, mu_] :=
+
+(* Whether partition lam is greater than or equal to partition mu, in lexicographical order *)
+PartitionGreaterEqual[lam_List, mu_List] :=
   MatchQ[#1 - #2 & @@ PadRight[{lam, mu}], {(0) ...} | {(0) ..., a_ /; a > 0, ___}];
 
-myc[kap_, kap_] := myc[kap, kap] =
-  (Multinomial @@ kap) - Total[myc[#, kap] & /@
+(* Define the coefficients c_{kappa,lambda} of zonal polynomials recursively. *)
+(* The diagonal coefficients c_{kappa,kappa} are computed with the "vertical" recursion. *)
+ZonalCoefficient[kap_List, kap_List] := ZonalCoefficient[kap, kap] =
+  (Multinomial @@ kap) - Total[ZonalCoefficient[#, kap]& /@
     With[{p = IntegerPartitions[Total[kap]]}, Take[p, Position[p, kap][[1, 1]] - 1]]
   ];
-
-myc[kap_, lam_] := myc[kap, lam] =
+(* Once the diagonal coefficient c_{kappa,kappa} is known, *)
+(* the coefficients c_{kappa,lambda} in the same row can be computed recursively. *)
+ZonalCoefficient[kap_List, lam_List] := ZonalCoefficient[kap, lam] =
 Module[{mus, sum},
   If[Min[FoldList[Plus, 0, Total[PadRight[{kap, -lam}]]]] < 0, Return[0]];
   mus = Flatten[Table[
@@ -22,44 +44,63 @@ Module[{mus, sum},
   (* We first accumulate their possible contributions *)
   (* and then perform the necessary checks only once. *)
   sum = Total[With[{mu = #[[1, 1]]},
-    If[pgeq[kap, mu], Total[Last /@ #] * myc[kap, mu], 0]]& /@
+    If[PartitionGreaterEqual[kap, mu], Total[Last /@ #] * ZonalCoefficient[kap, mu], 0]]& /@
     SplitBy[SortBy[mus, First], First]];
   Return[If[sum != 0, sum / (Rho[kap] - Rho[lam]), 0]];
 ];
 
-TableC[n_] :=
+(* Generates a table with all zonal polynomials coefficients c_{kappa,lambda}, *)
+(* where kappa and lambda are partitions of n. Hence the rows and columns *)
+(* of this matrix are indexed by IntegerPartitions[n]. *)
+ZonalCoefficientTable[n_Integer] :=
 Module[{c, part = IntegerPartitions[n], rk, mus, sum, pos, tab = {}},
   MapIndexed[Function[{kap, p},
     pos = p[[1]];
-    If[kap === {n}, c[{n}] = 1, c[kap] = Multinomial @@ kap - Total[#[[pos]] & /@ tab]];
+    If[kap === {n}, c[{n}] = 1, c[kap] = Multinomial @@ kap - Total[#[[pos]]& /@ tab]];
     rk = Rho[kap];
     Function[{lam},
-       mus = Flatten[Table[
-       {Sort[ReplacePart[lam, {i -> lam[[i]] + t, j -> lam[[j]] - t}], Greater],
-         lam[[i]] - lam[[j]] + 2 t},
-       {j, 2, Length[lam]}, {i, j - 1}, {t, lam[[j]]}], 2];
-       sum = Total[
-         With[{mu = DeleteCases[#[[1, 1]], 0]}, 
-           If[pgeq[kap, mu], Total[Last /@ #] * c[mu], 0]
-         ]& /@
-         SplitBy[SortBy[mus, First], First]
-       ];
-       c[lam] = If[sum != 0, sum/(rk - Rho[lam]), 0];
+      c[lam] = If[Min[FoldList[Plus, 0, Total[PadRight[{kap, -lam}]]]] < 0,
+        0
+      ,
+        mus = Flatten[Table[
+          {Sort[ReplacePart[lam, {i -> lam[[i]] + t, j -> lam[[j]] - t}], Greater],
+            lam[[i]] - lam[[j]] + 2 t},
+          {j, 2, Length[lam]}, {i, j - 1}, {t, lam[[j]]}], 2];
+        sum = Total[
+          With[{mu = DeleteCases[#[[1, 1]], 0]}, 
+            If[PartitionGreaterEqual[kap, mu], Total[Last /@ #] * c[mu], 0]
+          ]& /@
+          SplitBy[SortBy[mus, First], First]
+        ];
+        If[sum != 0, sum/(rk - Rho[lam]), 0]
+      ];
     ] /@ Drop[part, pos];
     AppendTo[tab, Join[Table[0, {pos - 1}], c /@ Drop[part, pos - 1]]];
   ], part];
   Return[tab];
 ];
 
-SymMon[lam_, y_] := 
+(* Gives the symmetric monomial function indexed by the partition lam, *)
+(* in the variables y = {y1, y2, ..., ym}. *)
+SymmetricMonomial[lam_List, y_List] := 
   If[Length[lam] > Length[y], 0,
     Total[(Times @@ (y^#))& /@ Permutations[PadRight[lam, Length[y]]]]
   ];
-ZonalC[lam_] := 
-  Total[(myc[lam, #] * (Sym @@ #))& /@ 
+
+(* Gives the zonal polynomial indexed by the partition lambda, *)
+(* in terms of the monomial symmetric functions. Example: *)
+(* ZonalPolynomial[{3, 2}] yields (48/7)*M[3, 2] + (176/21)*M[2, 2, 1] + *)
+(* (32/7)*M[3, 1, 1] + (64/7)*M[2, 1, 1, 1] + (80/7)*M[1, 1, 1, 1, 1] *)
+ZonalPolynomial[lam_List] := 
+  Total[(ZonalCoefficient[lam, #] * (M @@ #))& /@ 
     With[{p = IntegerPartitions[Total[lam]]}, Drop[p, Position[p, lam][[1, 1]] - 1]]
   ];
-ZonalC[lam_, y_] := ZonalC[lam] /. Sym[p__] :> SymMon[{p}, y];
+
+(* Gives the zonal polynomial indexed by the partition lambda, *)
+(* as a symmetric polynomial in the variables y. Example: *)
+(* ZonalPolynomial[{2,1}, {a,b,c}] yields *)
+(* (18/5)*a*b*c + (12/5)*(a^2*b + a*b^2 + a^2*c + b^2*c + a*c^2 + b*c^2). *)
+ZonalPolynomial[lam_List, y_List] := ZonalPolynomial[lam] /. M[p__] :> SymmetricMonomial[{p}, y];
 
 (* Requires the HolonomicFunctions package *)
 LaplaceBeltrami[y_List] := 
@@ -69,14 +110,19 @@ LaplaceBeltrami[y_List] :=
     {i, Length[y]}]
   ];
 
-
-mycn[kap_, kap_] := mycn[kap, kap] =
+(* Gives a symbolic expression (rational function in n) for the *)
+(* for the zonal coefficient c_{kappa,lambda} in the upper left corner. *)
+(* The partitions kappa and lambda must be of the form {n-i, pi}, *)
+(* where pi is a partition of i and where n is symbolic. Example: *)
+(* ZonalCoefficientN[{n-3,2,1},{n-4,2,2}] gives *)
+(* (4*(-3 + n)*(-1 + n)*n*(39 - 18*n + 2*n^2))/(5*(-11 + 2*n)*(-7 + 2*n)) *)
+ZonalCoefficientN[kap_List, kap_List] := ZonalCoefficientN[kap, kap] =
 Module[{p},
   p = Flatten[Table[Prepend[#, n - i]& /@ IntegerPartitions[i], {i, 0, n - kap[[1]]}], 1];
   p = Take[p, Position[p, kap][[1, 1]] - 1];
-  Return[Factor[FunctionExpand[Multinomial @@ kap] - Total[mycn[#, kap] & /@ p]]];
+  Return[Factor[FunctionExpand[Multinomial @@ kap] - Total[ZonalCoefficientN[#, kap]& /@ p]]];
 ];
-mycn[kap_, lam_] := mycn[kap, lam] =
+ZonalCoefficientN[kap_List, lam_List] := ZonalCoefficientN[kap, lam] =
 Module[{mus, sum},
   mus = Flatten[Table[
     {Sort[DeleteCases[ReplacePart[lam, {i -> lam[[i]] + t, j -> lam[[j]] - t}], 0], Greater],
@@ -86,7 +132,7 @@ Module[{mus, sum},
   (* We first accumulate their possible contributions *)
   (* and then perform the necessary checks only once. *)
   sum = Total[With[{mu = #[[1, 1]]}, 
-    If[pgeq[kap, mu], Total[Last /@ #] * mycn[kap, mu], 0]]& /@
+    If[PartitionGreaterEqual[kap, mu], Total[Last /@ #] * ZonalCoefficientN[kap, mu], 0]]& /@
     SplitBy[SortBy[mus, First], First]];
   Return[If[sum =!= 0, Factor[sum / (Rho[kap] - Rho[lam])], 0]];
 ];
